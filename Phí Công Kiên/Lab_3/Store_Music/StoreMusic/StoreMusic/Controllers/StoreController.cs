5457261b59606aaf50ad7f38342b2ea0967f7d95
phí kiên <44530636+PhiKien@users.noreply.github.com>
2019-06-21 23:38:25 +0700

    update v2.4

---
+++ Phí Công Kiên/Lab_3/Store_Music/StoreMusic/StoreMusic/Controllers/StoreController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StoreMusic.Models;
+
+namespace StoreMusic.Controllers
+{
+    public class StoreController : Controller
+    {
+        private StoreMusicContext db = new StoreMusicContext();
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View(db.Genres.ToList());
+        }
+
+        // GET: /Store/Browse?genre=Disco
+
+        public ActionResult Browse(string genre)
+        {
+            // Retrieve Genre and its Associated Albums from database
+            var genreModel = db.Genres.Include("Albums")
+                .Single(g => g.Name == genre);
+
+            return View(genreModel);
+        }
+
+        // GET: /Store/GenreMenu
+
+        [ChildActionOnly]
+        public ActionResult GenreMenu()
+        {
+            var genres = db.Genres.ToList();
+
+            return PartialView(genres);
+        }
+
+        // GET: Store/Details/5
+        public ActionResult Details(int id)
+        {
+            var album = db.Albums.Find(id);
+
+            return View(album);
+        }
+
+        // GET: Store/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Store/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GenreId,Name,Description")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Genres.Add(genre);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(genre);
+        }
+
+        // GET: Store/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = db.Genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        // POST: Store/Edit/5
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GenreId,Name,Description")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(genre).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(genre);
+        }
+
+        // GET: Store/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = db.Genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        // POST: Store/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Genre genre = db.Genres.Find(id);
+            db.Genres.Remove(genre);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
