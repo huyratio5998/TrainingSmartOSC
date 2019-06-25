d6bc9abfb812e19e8698aba2a6fb79ede3dcdefa
huyratio <huynguyen98.clc@gmail.com>
2019-06-25 14:09:53 +0700

    commit thu

---
+++ Phí Công Kiên/Lab_3/Store_Music_23062019_finish_full/Store_Music/Store_Music/ViewModels/ShoppingCartRemoveViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Store_Music.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Store_Music.ViewModels
+{
+    public class ShoppingCartRemoveViewModel
+    {
+        public string Message { get; set; }
+        public decimal CartTotal { get; set; }
+        public int CartCount { get; set; }
+        public int ItemCount { get; set; }
+        public int DeleteId { get; set; }
+    }
+}
