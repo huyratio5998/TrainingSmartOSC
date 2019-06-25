ce23426fc56eda6ffdd18cd5e42e34a0aea25b8c
vuductung1995 <39178546+vuductung1995@users.noreply.github.com>
2019-06-25 14:17:19 +0700

    update all

---
+++ Phí Công Kiên/Lab_3/Store_Music/StoreMusic/StoreMusic/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using StoreMusic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreMusic.Controllers
+{
+    public class HomeController : Controller
+    {
+        private StoreMusicContext storeDB;
+
+        public HomeController()
+        {
+            storeDB = new StoreMusicContext();
+        }
+        public ActionResult Index()
+        {
+            var albums = storeDB.Albums.ToList();
+            return View(albums);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+    }
+}
