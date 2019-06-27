76588a85933552761439d9aca5549040204bb5f0
huyratio <huynguyen98.clc@gmail.com>
2019-06-27 13:52:40 +0700

    fix conflict

---
+++ Nguyễn Văn Huy/Ajax/VDdemo/VDdemo/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VDdemo.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string fullname { get; set; }
+        public string gender { get; set; }
+    }
+}
