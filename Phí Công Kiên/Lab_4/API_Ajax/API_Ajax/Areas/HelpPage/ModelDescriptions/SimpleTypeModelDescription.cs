41d6c98fbbb3d6627c16c3d9f40f217c88e977f6
PhiKien <44530636+PhiKien@users.noreply.github.com>
2019-06-28 11:49:54 +0700

    Update: Lab_4

---
+++ Phí Công Kiên/Lab_4/API_Ajax/API_Ajax/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API_Ajax.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Phí Công Kiên/Lab_4/API_Ajax/API_Ajax/Models/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace API_Ajax.Models
+{
+    public class Cart
+    {
+        public Cart()
+        {
+            this.Pcs = new HashSet<ProductCart>();
+        }
+        [Key]
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Buyer { get; set; }
+        public int TotalPrice { get; set; }
+        public virtual ICollection<ProductCart> Pcs { get; set; }
+    }
+}
